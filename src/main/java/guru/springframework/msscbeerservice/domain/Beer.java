86699a4276bd88e19d47d638a895e307dada350c
in3des <weabs@mail.ru>
2022-04-28 15:37:25 +0400

    Fixing build issues.

--- src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -1,30 +1,43 @@
 package guru.springframework.msscbeerservice.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Data
+@Entity
 public class Beer {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
 
+    @Version
     private Integer version;
 
+    @CreationTimestamp
+    @Column(updatable = false)
     private Timestamp createdDate;
+
+    @UpdateTimestamp
     private Timestamp lastModifiedDate;
 
     private String beerName;
     private String beerStyle;
 
+    @Column(unique = true)
     private Long upc;
 
     private BigDecimal price;
